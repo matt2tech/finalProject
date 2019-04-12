3f2ce0719add89400360ef0a120f70b91c7f4ccf
matt2tech <mlipsey@basecampcodingacademy.org>
2019-04-12 15:18:36 -0500

    Function arrayBuild builds an array with length

--- core.js
+++ core.js
@@ -1,5 +1,4 @@
 function quickSort(array) {
-    var final = [];
     var less = [];
     var greater = [];
 
@@ -8,9 +7,6 @@ function quickSort(array) {
     } else {
         var pivot = array[0];
 
-        // var less = array[1:].filter(x => (x <= pivot)).map(x => x)
-        // var greater = array.filter(x => (x > pivot)).map(x => x)
-
         for(var i = 1; i < array.length; i++) {
             if (array[i] <= pivot) {
                 less.push(array[i])
@@ -18,51 +14,24 @@ function quickSort(array) {
                 greater.push(array[i])
             }
         }
-
-        // var less = lessSort(array, pivot);
-        console.log("Less: " + less);
-        // var greater = greaterSort(array, pivot);
-        console.log("Greater: " + greater);
-
-        // final.push(quickSort(less));
-        // final.push(pivot);
-        // final.push(quickSort(greater));
         return quickSort(less).concat([pivot].concat(quickSort(greater)));
     }
 }
 
-function lessSort(array, pivot) {
-    var list = [];
-    if (array.length < 2) {
-        return list;
-    } else {
-        for (var i = 1; i <= array.length; i++) {
-            if (array[i] <= pivot) {
-                list.push(array[i]);
-            } else {
-                continue;
-            }
-        }
-        return list;
+function arrayBuild(length) {
+    array = []
+    for(var i = 0; i < length; i++) {
+        array.push(Math.ceil(Math.random() * length))
     }
-}
-
-function greaterSort(array, pivot) {
-    var list = [];
-    if (array.length < 2) {
-        return list;
-    } else {
-        for (var i = 1; i <= array.length; i++) {
-            if (array[i] > pivot) {
-                list.push(array[i]);
-            } else {
-                continue;
-            }
-        }
-        return list;
-    }
+    return array
 }
 
 var array = [10, 5, 2, 3];
 
-console.log("end result: " + quickSort(array));
+var arrayLength = Math.ceil(Math.random() * 10);
+
+var array = arrayBuild(arrayLength)
+
+console.log("Length: " + arrayLength);
+console.log("Array: " + array);
+console.log("Quicksort: " + quickSort(array));
