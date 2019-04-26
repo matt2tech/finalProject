d2832f365c1ab30826355f6de4e85ec143dd41a2
matt2tech <mlipsey@basecampcodingacademy.org>
2019-04-26 11:31:19 -0500

    Changed some text and style

--- core.js
+++ core.js
@@ -6,7 +6,6 @@ var Quick = [];
 var Bubble = [];
 var Select = [];
 
-
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
@@ -14,7 +13,7 @@ var xScale;
 var yScale;
 var sections;
 
-var xAxis = [""]
+var xAxis = [""];
 
 // listens to clear button. clears data and page
 document.getElementById("clear").addEventListener("click", function() {
@@ -43,7 +42,7 @@ document.getElementById("clear").addEventListener("click", function() {
     context = canvas.getContext("2d");
     xScale;
     yScale;
-    xAxis = [""]
+    xAxis = [""];
 });
 
 // listens to build button to run arrayBuild function
@@ -58,7 +57,7 @@ document.getElementById("build").addEventListener("click", function() {
         build.setAttribute("disabled", "true");
         build.innerText = "Building";
 
-        worker.postMessage({type: "build", data: length});
+        worker.postMessage({ type: "build", data: length });
         worker.onmessage = function(event) {
             globalArray = event.data.array;
             console.log("Array: " + globalArray);
@@ -67,7 +66,7 @@ document.getElementById("build").addEventListener("click", function() {
 
             document.getElementById("build").innerText = "Built";
             document.getElementById("clear").removeAttribute("disabled");
-        }
+        };
     } else {
         console.log("Invalid number");
     }
@@ -76,7 +75,7 @@ document.getElementById("build").addEventListener("click", function() {
 // listens to quick button and runs quicksort function
 document.getElementById("quick").addEventListener("click", function() {
     const worker = new Worker("workers/quick.js");
-    const quick = document.getElementById("quick")
+    const quick = document.getElementById("quick");
 
     if (globalArray.length > 0) {
         quick.setAttribute("disabled", "true");
@@ -85,12 +84,7 @@ document.getElementById("quick").addEventListener("click", function() {
         worker.postMessage({ type: "quicksort", data: globalArray });
         worker.onmessage = function(event) {
             document.getElementById("quickPerform").innerText =
-            "Time Indices: " +
-            event.data.timeArray.length +
-                " indices" +
-                "\nTime: " +
-                event.data.time +
-                " milliseconds";
+                "Time: " + event.data.time + " milliseconds";
             quick.innerText = "Done";
             Quick = event.data.timeArray;
             context.strokeStyle = "#007bff";
@@ -113,12 +107,7 @@ document.getElementById("bubble").addEventListener("click", function() {
         worker.postMessage({ type: "bubblesort", data: globalArray });
         worker.onmessage = function(event) {
             document.getElementById("bubblePerform").innerText =
-            "Time Indices: " +
-            event.data.timeArray.length +
-                " indices" +
-                "\nTime: " +
-                event.data.time +
-                " milliseconds";
+                "Time: " + event.data.time + " milliseconds";
             bubble.innerText = "Done";
             Bubble = event.data.timeArray;
             context.strokeStyle = "#dc3545";
@@ -141,12 +130,7 @@ document.getElementById("select").addEventListener("click", function() {
         worker.postMessage({ type: "selectsort", data: globalArray });
         worker.onmessage = function(event) {
             document.getElementById("selectPerform").innerText =
-                "Time Indices: " +
-                event.data.timeArray.length +
-                " indices" +
-                "\nTime: " +
-                event.data.time +
-                " milliseconds";
+                "Time: " + event.data.time + " milliseconds";
             select.innerText = "Done";
             Select = event.data.timeArray;
             context.strokeStyle = "#28a745";
@@ -159,49 +143,48 @@ document.getElementById("select").addEventListener("click", function() {
 
 // builds an empty graph
 function buildGraph() {
-            // set these values for your data
-            sections = 10;
-            Val_max = 100;
-            Val_min = 0;
-            var stepSize = 10;
-            var columnSize = 50;
-            var rowSize = 50;
-            var margin = 5;
+    // set these values for your data
+    sections = 10;
+    Val_max = 100;
+    Val_min = 0;
+    var stepSize = 10;
+    var columnSize = 50;
+    var rowSize = 50;
+    var margin = 5;
 
-            buildXAxis();
+    buildXAxis();
 
-            context.fillStyle = "#0099ff";
-            context.font = "20 pt Verdana";
+    context.fillStyle = "#0099ff";
+    context.font = "20 pt Verdana";
 
-            yScale =
-                (canvas.height - columnSize - margin) / (Val_max - Val_min);
-            xScale = (canvas.width - rowSize) / sections;
+    yScale = (canvas.height - columnSize - margin) / (Val_max - Val_min);
+    xScale = (canvas.width - rowSize) / sections;
 
-            context.strokeStyle = "#000"; // color of grid lines
-            context.beginPath();
-            // print Parameters on X axis, and grid lines on the graph
-            for (i = 1; i <= sections; i++) {
-                var x = i * xScale;
-                context.fillText(xAxis[i], x, columnSize - margin);
-                context.moveTo(x, columnSize);
-                context.lineTo(x, canvas.height - margin);
-            }
-            // print row header and draw horizontal grid lines
-            var count = 0;
-            for (scale = Val_max; scale >= Val_min; scale = scale - stepSize) {
-                var y = columnSize + yScale * count * stepSize;
-                context.fillText(scale, margin, y + margin);
-                context.moveTo(rowSize, y);
-                context.lineTo(canvas.width, y);
-                count++;
-            }
-            context.stroke();
+    context.strokeStyle = "#000"; // color of grid lines
+    context.beginPath();
+    // print Parameters on X axis, and grid lines on the graph
+    for (i = 1; i <= sections; i++) {
+        var x = i * xScale;
+        context.fillText(xAxis[i], x, columnSize - margin);
+        context.moveTo(x, columnSize);
+        context.lineTo(x, canvas.height - margin);
+    }
+    // print row header and draw horizontal grid lines
+    var count = 0;
+    for (scale = Val_max; scale >= Val_min; scale = scale - stepSize) {
+        var y = columnSize + yScale * count * stepSize;
+        context.fillText(scale, margin, y + margin);
+        context.moveTo(rowSize, y);
+        context.lineTo(canvas.width, y);
+        count++;
+    }
+    context.stroke();
 
-            context.translate(rowSize, canvas.height + Val_min * yScale);
-            context.scale(1, -1 * yScale);
+    context.translate(rowSize, canvas.height + Val_min * yScale);
+    context.scale(1, -1 * yScale);
 
-            document.getElementById("chart").removeAttribute("hidden");
-        }
+    document.getElementById("chart").removeAttribute("hidden");
+}
 
 // plots data points on constructed graph
 function plotData(dataSet) {
@@ -218,8 +201,8 @@ function plotData(dataSet) {
 
 // builds xAxis for the graph based on globalArray length
 function buildXAxis() {
-    for(var i = 0; i <= globalArray.length; i += globalArray.length * 0.1) {
-            i = parseFloat(i.toFixed(1));
-            xAxis.push(i);
+    for (var i = 0; i <= globalArray.length; i += globalArray.length * 0.1) {
+        i = parseFloat(i.toFixed(1));
+        xAxis.push(i);
     }
 }
